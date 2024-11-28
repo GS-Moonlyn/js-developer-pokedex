7be3b23022c40c86acd80b3311cce6dc871739a7
Gabriel Oliveira <gsmolive@gmail.com>
2024-11-27 23:43:06 -0300

    Projeto Finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,87 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+const detailsContainer = document.getElementById('pokemonDetails');
 
 const maxRecords = 151
 const limit = 10
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
+    const pokemonHash = `#pokemon-${pokemon.number}`;
+
     return `
         <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
+            <button class="pokemon-button" onclick="navigateToPokemon('${pokemonHash}')">
+                <div class="pokemoHeader">
+                <span class="name">${pokemon.name}</span>
+                <span class="number">#${pokemon.number}</span>
+                </div>
 
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
 
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+            </button>
         </li>
-    `
+    `;
+}
+
+function navigateToPokemon(pokemonHash) {
+    window.location.hash = pokemonHash;
+
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+function loadPokemonDetails() {
+    const pokemonHash = window.location.hash;
+
+    if (!pokemonHash) {
+        return;
+    }
+
+    if (pokemonHash) {
+        const pokemonId = pokemonHash.replace('#pokemon-', '');
+
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+            .then(response => response.json())
+            .then(pokemon => {
+
+                const abilitiesList = pokemon.abilities.map(ability => ability.ability.name).join(' / ');
+
+                detailsContainer.innerHTML = `
+                    <h2>${pokemon.name}</h2>
+                    <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+                    <p>Types: ${pokemon.types.map(type => type.type.name).join(' / ')}</p>
+                    <p>Height: ${pokemon.height / 10} <span class = "measures">m</span></p>
+                    <p>Weight: ${pokemon.weight / 10} <span class = "measures">kg</span></p>
+                    <p>Abilties: ${abilitiesList}</p>
+                    <button onclick="hideDetails()">Hide</button>
+                `;
+            })
+            .catch(err => {
+                console.error('Error fetching Pokémon details:', err);
+            });
+    }
+}
+
+function hideDetails() {
+    window.location.hash = ''; 
+    
+    detailsContainer.innerHTML = '';
+
+    pokemonList.scrollIntoView({ behavior: 'smooth' });
+}
+
+window.addEventListener('hashchange', loadPokemonDetails);
+
+if (window.location.hash) {
+    loadPokemonDetails();
 }
 
 function loadPokemonItens(offset, limit) {
@@ -31,6 +92,7 @@ function loadPokemonItens(offset, limit) {
 }
 
 loadPokemonItens(offset, limit)
+
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit
